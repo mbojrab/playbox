ac651ad08d96c0e39020b84c2618658725e6f28e
mbojrab <micah.bojrab@mdaus.com>
2016-03-24 23:59:17 -0400

    Updates to get the coinCounter working to a better degree.

--- trunk/projects/coinCounter/apps/coinCounter.py
+++ trunk/projects/coinCounter/apps/coinCounter.py
@@ -5,7 +5,7 @@ import numpy as np
 #from nn.net import ClassifierNetwork as Net
 from nn.net import ClassifierNetwork as Net
 from nn.datasetUtils import convertImageToNumpy
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import theano
 
 coinValue = [.10, .0, .05, .01, .25]
@@ -40,7 +40,7 @@ def countCoins(image, batchSet, show=False, debug=False) :
 
     # run the coins through the network to get their value
     ii = 0
-    totalValue = 0.
+    results = []
     if circles is not None :
         for c in circles[0,:] :
             # chip the coin
@@ -65,38 +65,40 @@ def countCoins(image, batchSet, show=False, debug=False) :
             chipPosition = ii % batchSet.shape[0]
             batchSet[chipPosition] = convertImageToNumpy(chip, True)
             if debug :
-                cv2.imwrite('chipNP_' + str(ii) + '.tiff', batchSet[chipPosition,0,:,:]*255.)
+                cv2.imwrite('chipNP_' + str(ii) + '.tiff', 
+                            batchSet[chipPosition,0,:,:]*255.)
 
             # once the buffer is full classify it
             if (ii+1) % batchSet.shape[0] == 0 :
-                result = network.classify(batchSet)
-                totalValue += sum([coinValue[cl] for cl in result])
-                if debug :
-                    print [network.getNetworkLabels()[cl] for cl in result]
-                    print result
-
+                results.extend(network.classify(batchSet))
             if debug :
                 chip.save('chip_' + str(ii) + '.tiff')
             ii += 1
 
+        # add any partially filled batches
         remainder = len(circles[0]) % batchSet.shape[0]
         if remainder > 0:
-            result = network.classify(batchSet)[:remainder]
-            totalValue += sum([coinValue[cl] for cl in result])
-            if debug :
-                print [network.getNetworkLabels()[cl] for cl in result]
-                print result
+            results.extend(network.classify(batchSet)[:remainder])
 
-
+    totalValue = sum([coinValue[cl] for cl in results])
 
     if show :
-        # for diagnostics -- draw boxes and display the results
         draw = ImageDraw.Draw(image)
+        labels = [network.getNetworkLabels()[cl] for cl in results]
+
+        # for diagnostics -- draw boxes and display the results
         if circles is not None :
+            ii = 0
+            font = ImageFont.truetype("Stencil.ttf", 16)
             for c in circles[0,:] :
                 # draw a box on the image to indicate we found the coin
                 radius, upperLeft, lowerRight = getChipArea(c)
-                draw.rectangle([upperLeft, lowerRight], outline=(0,255,0))
+                draw.rectangle([upperLeft, lowerRight], outline=(0,150,0))
+
+                # put the label over the object
+                draw.text([c[0]-(c[2]/1.2), c[1]], labels[ii],
+                          font=font, fill=(150,0,200))
+                ii += 1
             cv2.imshow('update', 
                        cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR))
 
@@ -161,7 +163,7 @@ if __name__ == '__main__' :
 
         # setup the video device
         videoCap = cv2.VideoCapture(options.device)
-        videoCap.set(cv2.cv.CV_CAP_PROP_EXPOSURE, .05) 
+        videoCap.set(cv2.cv.CV_CAP_PROP_EXPOSURE, .05)
 
         # start capturing frames
         ii = 0
@@ -172,7 +174,7 @@ if __name__ == '__main__' :
             ret, frame = videoCap.read()
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             totalValues[ii%captureMem] = countCoins(Image.fromarray(frame), 
-                                                    batchSet)
+                                                    batchSet, True)
             ii += 1
             print "Total Value: $" + str(mode(totalValues)[0][0])
 
