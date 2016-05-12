78c04e6cf4f64f7c5f3f87de61d0db5e591e362a
mbojrab <micah.bojrab@mdaus.com>
2016-05-11 23:17:11 -0400

    moving deinterleaveTuples to chip.py and renaming to makeContiguous

--- trunk/modules/python/dataset/chip.py
+++ trunk/modules/python/dataset/chip.py
@@ -1,3 +1,24 @@
+def makeContiguous(x) :
+    '''Disentangles the tuple, such that each is contigous in a list.
+       x      : list of tuples to deinterleave. All objects are assumed to be
+                arrays, which will be combined into a single tensor
+       return : tuple of tensors. The length of the tuple will equal that of
+                the individual members in the input
+    '''
+    import numpy as np
+    numTuples = len(x)
+    if numTuples == 0 :
+        raise Exception('Cannot deinterleave an empty list.')
+    numElems = len(x[0])
+
+    # extract each item into contiguous memory tensor
+    ret = []
+    temp = np.concatenate(x)
+    for ii in range(numElems) :
+        ret.append(np.resize(np.concatenate(temp[ii::numElems]),
+                             tuple([numTuples] + list(temp[0][ii].shape))))
+    return ret
+
 def grid(image, chipSize, skipFactor=0, pixelRegion=False, log=None):
     '''This chips the region into non-overlapping sub-regions. All partial
        border chips will be disgarded from the returned array.
@@ -27,14 +48,12 @@ def overlapGrid(image, chipSize, stepFactor,
                      (startRow,startCol,endRow,endCol)
        log         : Logger to use
     '''
-    import numpy as np
-
     if log is not None :
         log.info('Subdividing the Image')
 
     # grab an grid of chips
     chips = []
-    chipRows, chipCols, numChannels = chipSize[0], chipSize[1], image.shape[0]
+    chipRows, chipCols = chipSize[0], chipSize[1]
     for row in range(0, image.size[1] - chipSize, stepFactor[0]) :
         for col in range(0, image.size[2] - chipSize, stepFactor[1]) :
             chips.append((image[:, row : row + chipRows, col : col + chipCols],
@@ -43,15 +62,8 @@ def overlapGrid(image, chipSize, stepFactor,
     # make data contigous in memory --
     # all pixels will be arranged into a single tensor of size 
     # (numChips,numChannels,chipRows,chipCols), (numChips,4)
-    # TODO: use minibatch.deinterleaveTuples()
-    numChips = len(chips)
-    chips = np.concatenate(chips)
-    pixels = np.resize(np.concatenate(chips[::2]),
-                      (numChips,numChannels,chipRows,chipCols))
-
-    # create a matrix for the pixel regions if requested
-    regions = None
-    if pixelRegion :
-        np.resize(np.concatenate(chips[1::2]), (numChips, 4))
+    pixels, regions = makeContiguous(chips)
+    if not pixelRegion :
+        regions = None
 
     return pixels, regions
