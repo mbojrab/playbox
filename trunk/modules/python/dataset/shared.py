2ffcd487125f8b793947a6ed9ac27a3094079c9f
mbojrab <micah.bojrab@mdaus.com>
2016-05-12 00:32:39 -0400

    adding better logging throughout

--- trunk/modules/python/dataset/shared.py
+++ trunk/modules/python/dataset/shared.py
@@ -1,15 +1,18 @@
 import theano as t
 
-def loadShared(x, borrow=True) :
+def loadShared(x, borrow=True, log=None) :
     '''Transfer numpy.array to theano.shared variable.
        NOTE: Shared variables allow for optimized GPU execution
     '''
     import numpy as np
+    if log is not None :
+        log.debug('Wrap memory into shared variables')
+
     if not isinstance(x, np.ndarray) :
         x = np.asarray(x, dtype=t.config.floatX)
     return t.shared(x, borrow=borrow)
 
-def splitToShared(x, borrow=True) :
+def splitToShared(x, borrow=True, log=None) :
     '''Create shared variables for both the input and expectedOutcome vectors.
        x      : This can be a vector list of inputs and expectedOutputs. It is
                 assumed they are of the same length.
@@ -19,4 +22,5 @@ def splitToShared(x, borrow=True) :
                     Format - (data, label)
     '''
     data, label = x
-    return (loadShared(data), t.tensor.cast(loadShared(label), 'int32'))
+    return (loadShared(data, log), 
+            t.tensor.cast(loadShared(label, log), 'int32'))
