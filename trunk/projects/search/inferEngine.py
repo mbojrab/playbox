5a8296d76e752fb4f3ab58ce86a31f02b1570701
bojrab <micah.bojrab@mdaus.com>
2016-06-22 11:59:11 -0400

    further implementing the inference engine connection to flask for serving the demo

--- trunk/projects/search/inferEngine.py
+++ trunk/projects/search/inferEngine.py
@@ -2,40 +2,41 @@ import numpy as np
 from mstarTrainingSetGenerator import grabChip, getResearchXML, parseXML
 from sarSearch import preProcessing, detectImage
 
-colors = [[(187,38,90),
-           (210,100,100),
-           (245,45,38),
-           (340,1,94),
-           (261,11,90)],  
-          [(195,80,49),
-           (180,77,69),
-           (47,61,96),
-           (28,68,93),
-           (240,30,29)],
-          [(166,100,100),
-           (40,26,72),
-           (199,34,98),
-           (28,59,86),
-           (270,5,15)]]
-
-def publishWebpage() :
-    pass
+colors = [[[187,38,90],
+           [210,100,100],
+           [245,45,38],
+           [340,1,94],
+           [261,11,90]],  
+          [[195,80,49],
+           [180,77,69],
+           [47,61,96],
+           [28,68,93],
+           [240,30,29]],
+          [[166,100,100],
+           [40,26,72],
+           [199,34,98],
+           [28,59,86],
+           [270,5,15]]]
 
 def parseImage(image) :
     from dataset.reader import openSICD
 
     # read the objects from the research XML
-    objects = parseXML(getResearchXML(image)).getroot().iter("Object")
+    objects = parseXML(getResearchXML(image)).getroot().findall(".//Object")
 
     # read the wideband data
     wbData, _ = openSICD(options.image)
     return wbData, objects
 
-def classifyImage (network, batchSet, wbData, objects, opts, threads, log) :
+def classifyImage (network, batchSet, wbData, objects, opts, threads, log, imagePath) :
     #import cv2
+    import os
     import PIL.Image as Image
     from PIL import ImageDraw
     from dataset.reader import convertPhaseAmp
+
+    base = os.path.join(os.path.split(__file__)[0], 'webpage', 'static',
+                        os.path.basename(imagePath))
 
     # get the chip size
     chipSize = network.getNetworkInputSize()[2:]
@@ -45,11 +46,14 @@ def classifyImage (network, batchSet, wbData, objects, opts, threads, log) :
     for ii, observation in enumerate(objects) :
 
         # read the location and extract the chip
-        chip, loc = grabChip(wbData, observation, chipSize)
+        chip, loc = grabChip(wbData, observation, chipSize[0])
 
         # detect the chip for later
         chipDetected.append(detectImage(chip, opts, threads))
-        chipLocation.appen(loc)
+        chipLocation.append([loc[2], loc[0], loc[3], loc[1]])
+        Image.fromarray(chipDetected[-1]).save(
+            base.replace('SICD.nitf', '_' + str(ii) + '.jpeg'))
+
 
         # insert the chip into the pre-allocated buffer
         batchSet[ii % batchSet.shape[0]] = convertPhaseAmp(chip, log)
@@ -65,17 +69,23 @@ def classifyImage (network, batchSet, wbData, objects, opts, threads, log) :
 
     # detect the full image
     fullDetected = Image.fromarray(detectImage(wbData, opts, threads))
+    width = 500
+    factor = float(width)/fullDetected.size[0]
+    fullDetected.thumbnail((width, int(fullDetected.size[1] * factor)),
+                           Image.ANTIALIAS)
+    fullDetected = fullDetected.convert('RGB')
 
     # apply the locations to the overview image
     draw = ImageDraw.Draw(fullDetected)
-    for label, loc in zip([network.getNetworkLabels()[cl] for cl in results],
-                           chipLocation) :
+    for label, loc in zip(network.convertToLabels(results[0]), chipLocation) :
+        # draw a box around the object
         if label.upper() != "MISC" :
-
-            # draw a box around the object and add text
-            draw.rectangle([loc[0], loc[1]], outline=labelColors[label])
+            draw.rectangle([int(loc[0]*factor), int(loc[1]*factor),
+                            int(loc[2]*factor), int(loc[3]*factor)],
+                           fill=tuple(labelColors[label] + [5]))
 
     # return the results
+    fullDetected.save(base.replace('SICD.nitf', '_full.jpeg'))
     return (fullDetected, np.concatenate(chipDetected), results)
 
 
@@ -118,11 +128,6 @@ if __name__ == '__main__' :
     for ii, label in enumerate(network.getNetworkLabels()) :
         labelColors[label] = colors[options.color][ii]
 
-    # build the webpage
-    publishWebpage()
-
     # process the image(s)
     wbData, objects = parseImage(options.image)
-    classifyImage(network, batchSet, wbData, objects, opts, threads, log)
-
-    
+    classifyImage(network, batchSet, wbData, objects, opts, threads, log, options.image)
