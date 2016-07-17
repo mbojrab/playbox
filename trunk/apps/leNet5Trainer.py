30677cae698c1d4c00c5b2c94d8f27daab74d84a
mbojrab <micah.bojrab@mdaus.com>
2016-07-16 20:58:12 -0400

    ClassifierSAENetwork uses the ClassifierNetwork which provides it now the ability to identify the feature encodings for a particular input(s). This can be used as a low-accuracy classifier based of patterns identified during feature extraction training. StackedAENetwork is now renamed to TrainerSAENetwork for consistency with nn module. All apps have been updated to use this new network naming.

--- trunk/apps/leNet5Trainer.py
+++ trunk/apps/leNet5Trainer.py
@@ -125,4 +125,3 @@ if __name__ == '__main__' :
                     learnF=options.learnF, momentum=options.momentum, 
                     kernel=options.kernel, neuron=options.neuron, 
                     log=log)
-    del network
