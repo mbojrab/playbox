94c749d847e1918ec2d495a10ddb7bd91e82c096
mbojrab <micah.bojrab@mdaus.com>
2016-11-16 16:59:09 -0500

    combined training for both systems into a common hidden function. This is because both systems are now largely redundant with minor differences.

--- trunk/modules/python/nn/trainUtils.py
+++ trunk/modules/python/nn/trainUtils.py
@@ -1,6 +1,8 @@
 import os
 from dataset.writer import buildPickleInterim, buildPickleFinal, resumeEpoch
 from time import time
+from nn.net import TrainerNetwork
+from ae.net import TrainerSAENetwork
 
 def renameBestNetwork(lastSave, bestNetwork, log=None) :
     if log is not None :
@@ -12,9 +14,86 @@ def renameBestNetwork(lastSave, bestNetwork, log=None) :
 
     return bestNetwork
 
-# TODO: This is largely redundant with the method below. Look into combining
-#       them in an intuitive manner. There are minor differences between them
-#       so watch out for this!
+def _train(network, appName, dataPath, numEpochs=5, stop=1,
+           synapse=None, base=None, dropout=None, 
+           learnC=None, learnF=None, contrF=None, momentum=None, 
+           kernel=None, neuron=None, numLayers=1, log=None) :
+    '''This trains a stacked autoencoder in a greedy layer-wise manner. This
+       starts by train each layer in sequence for the specified number of
+       epochs, then returns the network. This can be used to initialize a
+       Neural Network into a decent initial state.
+
+       network : StackedAENetwork to used for training
+       return  : Path to the trained network. This will be used as a 
+                 pre-trainer for the Neural Network
+    '''
+    isSup = lambda x: isinstance(x, TrainerNetwork)
+    isImproved = lambda x, y, net : x > y if isSup(net) else x < y
+    getLayer = lambda x, net: None if isSup(net) else x
+
+    globalEpoch = lastBest = resumeEpoch(synapse)
+    lastSave = buildPickleInterim(
+        base=base, epoch=lastBest, dropout=dropout,
+        learnC=learnC, learnF=learnF, contrF=contrF,
+        momentum=momentum, kernel=kernel, neuron=neuron,
+        layer=getLayer(0, network))
+    network.save(lastSave)
+
+    # this loop allows the network to be trained layer-wise. The unsupervised
+    # learning method allows each layer to be trained individually.
+    for layerIndex in range(numLayers) :
+        degraded = 0
+        running = network.checkAccuracy() if isSup(network) else \
+                  network.checkReconstructionLoss(layerIndex)
+
+        # continue training until the network hits the early stoppage condition
+        while True :
+
+            # train the network and log the result
+            timer = time()
+
+            if isSup(network) :
+                globalEpoch = network.trainEpoch(globalEpoch, numEpochs)
+                current = network.checkAccuracy()
+                log.info('Checking Accuracy - {0}s ' \
+                         '\n\tCorrect   : {1}% \n\tIncorrect : {2}%'.format(
+                         time() - timer, current, (100-current)))
+            else :
+                globalEpoch, _ = network.trainEpoch(layerIndex, globalEpoch,
+                                                    numEpochs)
+                current = network.checkReconstructionLoss(layerIndex)
+                log.info('Checking Loss - {0}s ' \
+                         '\n\tCost   : {1}'.format(time() - timer, current))
+
+            # check if we've improved
+            if isImproved(current, running, network) :
+
+                # reset and save the network
+                degraded = 0
+                running = current
+                lastBest = globalEpoch
+                lastSave = buildPickleInterim(
+                    base=base, epoch=lastBest, dropout=dropout,
+                    learnC=learnC, learnF=learnF, contrF=contrF,
+                    momentum=momentum, kernel=kernel, neuron=neuron,
+                    layer=getLayer(layerIndex, network))
+                network.save(lastSave)
+            else :
+                # increment the number of lesser performing runs
+                degraded += 1
+
+            # stopping conditions for early stoppage
+            stopCondition = 100. if isSup(network) else 0.
+            if degraded > int(stop) or running == stopCondition :
+                break
+
+    # rename the network which achieved the highest accuracy
+    bestNetwork = buildPickleFinal(
+        base=base, appName=appName, dataName=os.path.basename(dataPath),
+        epoch=lastBest, accuracy=running if isSup(network) else None)
+    return renameBestNetwork(lastSave, bestNetwork, log)
+
+
 def trainUnsupervised(network, appName, dataPath, numEpochs=5, stop=1,
                       synapse=None, base=None, dropout=None, 
                       learnC=None, learnF=None, contrF=None, momentum=None, 
@@ -28,72 +107,11 @@ def trainUnsupervised(network, appName, dataPath, numEpochs=5, stop=1,
        return  : Path to the trained network. This will be used as a 
                  pre-trainer for the Neural Network
     '''
-    # train each layer in sequence --
-    # first we pre-train the data and at each epoch, we save each to disk
-    #
-    # the last iteration trains the network as a whole --
-    # this ensures the network fine-tunes its encodings wrt to all layers'
-    # encoding and decoding loss.
-    globalEpoch = lastBest = resumeEpoch(synapse)
-    lastSave = buildPickleInterim(base=base,
-                                  epoch=lastBest,
-                                  dropout=dropout,
-                                  learnC=learnC,
-                                  learnF=learnF,
-                                  contrF=contrF,
-                                  kernel=kernel,
-                                  neuron=neuron,
-                                  layer=0)
-    network.save(lastSave)
-
-    # train each layer individually
-    # this fully trains each layer before moving to next
-    for layerIndex in range(network.getNumLayers()) :
-        degradationCount = 0
-        runningCost = network.checkReconstructionLoss(layerIndex)
-
-        # continue training the layer until the network the network stops
-        # making progress or it hit a max limit
-        while True :
-
-            # train each layer -- greedily
-            timer = time()
-            globalEpoch, _ = network.trainEpoch(layerIndex, globalEpoch,
-                                                numEpochs)
-            elapsed = time() - timer
-            curCost = network.checkReconstructionLoss(layerIndex)
-            log.info('Checking Loss - {0}s - {1}'.format(elapsed, curCost))
-
-            # check if we've improved
-            if curCost < runningCost :
-
-                # reset and save the network
-                degradationCount = 0
-                runningCost = curCost
-                lastBest = globalEpoch
-                lastSave = buildPickleInterim(base=base,
-                                              epoch=lastBest,
-                                              dropout=dropout,
-                                              learnC=learnC,
-                                              learnF=learnF,
-                                              contrF=contrF,
-                                              kernel=kernel,
-                                              neuron=neuron,
-                                              layer=layerIndex)
-                network.save(lastSave)
-            else :
-                # increment the number of poor performing runs
-                degradationCount += 1
-
-            # stopping conditions for regularization
-            if degradationCount > int(stop) or runningCost == 0. :
-                break
-
-    # rename the network which achieved the highest accuracy
-    bestNetwork = buildPickleFinal(base=base, appName=appName,
-                                   dataName=os.path.basename(dataPath),
-                                   epoch=lastBest)
-    return renameBestNetwork(lastSave, bestNetwork, log)
+    _train(network=network, appName=appName, dataPath=dataPath,
+           numEpochs=numEpochs, stop=stop, synapse=synapse, base=base,
+           dropout=dropout, learnC=learnC, learnF=learnF, contrF=contrF,
+           momentum=momentum, kernel=kernel, neuron=neuron,
+           numLayers=network.getNumLayers(), log=log)
 
 def trainSupervised (network, appName, dataPath, numEpochs=5, stop=30, 
                      synapse=None, base=None, dropout=None, 
@@ -105,54 +123,7 @@ def trainSupervised (network, appName, dataPath, numEpochs=5, stop=30,
        return  : Path to the trained network. This will be used as a 
                  pre-trainer for the Neural Network
     '''
-    degradationCount = 0
-    globalEpoch = lastBest = resumeEpoch(synapse)
-    runningAccuracy = network.checkAccuracy()
-    lastSave = buildPickleInterim(base=base,
-                                  epoch=lastBest,
-                                  dropout=dropout,
-                                  learnC=learnC,
-                                  learnF=learnF,
-                                  momentum=momentum,
-                                  kernel=kernel,
-                                  neuron=neuron)
-    network.save(lastSave)
-    while True :
-        timer = time()
-
-        # run the specified number of epochs
-        globalEpoch = network.trainEpoch(globalEpoch, numEpochs)
-        # calculate the accuracy against the test set
-        curAcc = network.checkAccuracy()
-        log.info('Checking Accuracy - {0}s ' \
-                 '\n\tCorrect   : {1}% \n\tIncorrect : {2}%'.format(
-                 time() - timer, curAcc, (100-curAcc)))
-
-        # check if we've done better
-        if curAcc > runningAccuracy :
-            # reset and save the network
-            degradationCount = 0
-            runningAccuracy = curAcc
-            lastBest = globalEpoch
-            lastSave = buildPickleInterim(base=base,
-                                          epoch=lastBest,
-                                          dropout=dropout,
-                                          learnC=learnC,
-                                          learnF=learnF,
-                                          momentum=momentum,
-                                          kernel=kernel,
-                                          neuron=neuron)
-            network.save(lastSave)
-        else :
-            # increment the number of poor performing runs
-            degradationCount += 1
-
-        # stopping conditions for regularization
-        if degradationCount > int(stop) or runningAccuracy == 100. :
-            break
-
-    # rename the network which achieved the highest accuracy
-    bestNetwork = buildPickleFinal(base=base, appName=appName,
-                                   dataName=os.path.basename(dataPath),
-                                   epoch=lastBest, accuracy=runningAccuracy)
-    return renameBestNetwork(lastSave, bestNetwork, log)
+    _train(network=network, appName=appName, dataPath=dataPath,
+           numEpochs=numEpochs, stop=stop, synapse=synapse, base=base,
+           dropout=dropout, learnC=learnC, learnF=learnF,
+           momentum=momentum, kernel=kernel, neuron=neuron, log=log)
