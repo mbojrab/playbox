7630473d08e83197cddfac840ef851413776135d
mbojrab <micah.bojrab@mdaus.com>
2016-12-06 13:29:20 -0500

    update for a bit cleaner look

--- trunk/projects/unsupervised/classifySAETrainer.py
+++ trunk/projects/unsupervised/classifySAETrainer.py
@@ -48,9 +48,11 @@ def buildTrainerSAENetwork(network, layerInputSize, regType, regValue,
             # prepare for the next layer
             layerCount, layerInputSize = prepare(network, layerCount)
 
+    # add reset in case user uses removeLayer() logic
+    if len(network.getNumLayers()) > 0 :
+        layerInputSize = network.getNetworkOutputSize()
+
     # update to transition for fully connected layers
-    layerInputSize = network.getNetworkOutputSize() \
-                     if len(network.getNumLayers()) > 0 else layerInputSize
     layerInputSize = (layerInputSize[0], reduce(mul, layerInputSize[1:]))
     for n,l,m,dr,sc in zip(neuronFull, learnFull, momentumFull,
                            dropoutFull, sparseFull) :
