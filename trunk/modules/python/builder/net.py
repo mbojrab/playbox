e6798c65136f693811f537d8a02158c236118584
mbojrab <micah.bojrab@mdaus.com>
2016-12-09 15:12:19 -0500

    update to partition the builder function to provide improved generality.

--- trunk/modules/python/builder/net.py
+++ trunk/modules/python/builder/net.py
@@ -1,66 +1,109 @@
 import theano.tensor as t
-from nn.net import ClassifierNetwork
-from ae.encoder import AutoEncoder
-import numpy as np
+from numpy.random import RandomState
+from time import time
 
-def buildSAENetwork(network, layerInputSize, regType, regValue,
-                    kernelConv, kernelSizeConv, downsampleConv, 
-                    learnConv, momentumConv, dropoutConv, sparseConv,
-                    neuronFull, learnFull, momentumFull, dropoutFull,
-                    sparseFull, prof=None) :
-    '''Build the Stacked AutoEncoder network in an automated way.'''
-    from ae.contiguousAE import ContiguousAutoEncoder
+def addConvolutionalAE (network, inputSize, regType, regValue,
+                        kernel, kernelSize, downsample, 
+                        learnRate, momentumRate, dropout, forceSparsity,
+                        rng=None, prof=None) :
+    '''Add ConvolutionalAE layers to the network and return it.'''
     from ae.convolutionalAE import ConvolutionalAutoEncoder
 
-    from operator import mul
-    from numpy.random import RandomState
-    from time import time
-    rng = RandomState(int(time()))
+    # create a rng if one was not supplied
+    if rng is None :
+        rng = RandomState(int(time()))
 
-    if prof is not None :
-        prof.startProfile('Initialize the Network', 'info')
-
-    # prepare for the next layer
-    def prepare(network, count) :
-        return (count + 1, 
-                network.getNetworkOutputSize())
-
-    layerCount = 1
-    if kernelConv is not None :
-        for k,ks,do,l,m,dr,sc in zip(kernelConv, kernelSizeConv, 
-                                     downsampleConv, learnConv, momentumConv,
-                                     dropoutConv, sparseConv) :
-            # add a convolutional layer as defined
-            network.addLayer(ConvolutionalAutoEncoder(
-                layerID='conv' + str(layerCount), 
-                regType=regType, contractionRate=regValue,
-                inputSize=layerInputSize,
-                kernelSize=(k,layerInputSize[1],ks,ks),
-                downsampleFactor=[do,do], dropout=dr, 
-                learningRate=l, forceSparsity=sc, momentumRate=m,
-                activation=t.nnet.sigmoid, randomNumGen=rng))
-
-            # prepare for the next layer
-            layerCount, layerInputSize = prepare(network, layerCount)
-
-    # add reset in case user uses removeLayer() logic
+    # reset if the network already has layers
     if network.getNumLayers() > 0 :
-        layerInputSize = network.getNetworkOutputSize()
+        inputSize = network.getNetworkOutputSize()
 
-    # update to transition for fully connected layers
-    layerInputSize = (layerInputSize[0], np.prod(layerInputSize[1:]))
-    for n,l,m,dr,sc in zip(neuronFull, learnFull, momentumFull,
-                           dropoutFull, sparseFull) :
+    # TODO: Add default lists for momentumRate[0.], dropout[False], sparsity[True]
+    #       Just fail if kernelSize, downsample, or learnRate are not specified yet
+    #       kernel was specified.
+
+    # add each layer contiguously
+    for k,ks,do,l,m,dr,sc in zip(kernel, kernelSize, downsample,
+                                 learnRate, momentumRate,
+                                 dropout, forceSparsity) :
+
+        # add a convolutional layer as defined
+        network.addLayer(ConvolutionalAutoEncoder(
+            layerID='conv' + str(network.getNumLayers() + 1), 
+            regType=regType, contractionRate=regValue,
+            inputSize=inputSize,
+            kernelSize=(k,inputSize[1],ks,ks),
+            downsampleFactor=[do,do], dropout=dr, 
+            learningRate=l, forceSparsity=sc, momentumRate=m,
+            activation=t.nnet.sigmoid, randomNumGen=rng))
+
+        # prepare for the next layer
+        inputSize = network.getNetworkOutputSize()
+
+
+def addContiguousAE (network, inputSize, regType, regValue,
+                     neuron, learnRate, momentumRate, dropout, 
+                     forceSparsity, rng=None, prof=None) :
+    '''Add ContiguousAE layers to the network and return it.'''
+    import numpy as np
+    from ae.contiguousAE import ContiguousAutoEncoder
+
+    # create a rng if one was not supplied
+    if rng is None :
+        rng = RandomState(int(time()))
+
+    # reset if the network already has layers
+    if network.getNumLayers() > 0 :
+        inputSize = network.getNetworkOutputSize()
+
+    # flatten the input size -- just in case
+    inputSize = (inputSize[0], np.prod(inputSize[1:]))
+
+    # TODO: Add default lists for momentumRate[0.], dropout[False], sparsity[True]
+    #       Just fail if kernelSize, downsample, or learnRate are not specified yet
+    #       kernel was specified.
+
+    # add each layer contiguously
+    for n,l,m,dr,sc in zip(neuron, learnRate, momentumRate,
+                           dropout, forceSparsity) :
+
         # add a fully-connected layer as defined
         network.addLayer(ContiguousAutoEncoder(
-            layerID='fully' + str(layerCount),
+            layerID='fully' + str(network.getNumLayers() + 1),
             regType=regType, contractionRate=regValue,
-            inputSize=layerInputSize, numNeurons=n, learningRate=l,
+            inputSize=inputSize, numNeurons=n, learningRate=l,
             activation=t.nnet.sigmoid, dropout=dr, forceSparsity=sc,
             momentumRate=m, randomNumGen=rng))
 
         # prepare for the next layer
-        layerCount, layerInputSize = prepare(network, layerCount)
+        inputSize = network.getNetworkOutputSize()
+
+
+def buildSAENetwork(network, inputSize, regType, regValue,
+                    kernelConv, kernelSizeConv, downsampleConv, 
+                    learnConv, momentumConv, dropoutConv, sparseConv,
+                    neuronFull, learnFull, momentumFull, dropoutFull,
+                    sparseFull, rng=None, prof=None) :
+    '''Build the Stacked AutoEncoder network in an automated way.'''
+    from ae.contiguousAE import ContiguousAutoEncoder
+
+    # use the same random number generator across all layers for efficiency
+    if rng is None :
+        rng = RandomState(int(time()))
+
+    if prof is not None :
+        prof.startProfile('Initialize the Network', 'info')
+
+    # add any user-defined ConvolutionalAE layers first
+    addConvolutionalAE (network, inputSize, regType, regValue,
+                        kernelConv, kernelSizeConv, downsampleConv, 
+                        learnConv, momentumConv, dropoutConv, 
+                        sparseConv, rng=rng, prof=prof)
+
+
+    # add any user-defined ContiguousAE layers second
+    addContiguousAE (network, inputSize, regType, regValue,
+                     neuronFull, learnFull, momentumFull, dropoutFull, 
+                     sparseFull, rng=rng, prof=prof)
 
     if prof is not None :
         prof.endProfile()
