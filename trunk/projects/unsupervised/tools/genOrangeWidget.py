fc64fc520837ef320bbbef8db18819867bd4c2e1
micah bojrab <mlbojrab@gmail.com>
2017-03-19 23:26:57 -0400

    Initial add for this work. I started working on integrating pandas, but quickly realized that orange has its own interface, and it doesn't support pandas

---
+++ trunk/projects/unsupervised/tools/genOrangeWidget.py
@@ -0,0 +1,97 @@
+﻿import argparse
+from dataset.ingest.labeled import ingestImagery
+from builder.args import addLoggingParams, addUnsupDataParams
+from builder.profiler import setupLogging
+from ..classifySAEApp import createNetworks
+
+def sortDataset(netList, data, percentile=.95, debug=False) :
+    '''Test the imagery for how close it is to the target data. This also sorts
+       the results according to closeness, so we can create a tiled tip-sheet.
+    '''
+    from dataset.debugger import saveTiledImage
+    import numpy as np
+    import math
+    import pandas as pd
+
+    imagery = data[0]
+    labels = data[1]
+    sims = pd.DataFrame(columns=['Similarity', 'Batch', 'Index',
+                                 'Encoding', 'True Label'])
+    batchSize = imagery.shape[1]
+    for ii, batch in enumerate(imagery) :
+        # only store the encoding from the first network
+        cos, enc = nets[0].closenessAndEncoding(batch)
+
+        # average the results found by multiple networks
+        [net.closenessAndEncoding(batch, cos) for net in nets[1:]]
+        sims.append([ii // batchSize, ii % batchSize, cos / float(len(nets)),
+                    enc, labels[ii]], ignore_index=True)
+    sims.to_csv('output.csv')
+    import sys
+    sys.exit(0)
+
+    # rank their closeness from high to low
+    sims = [(ii // batchSize, ii % batchSize, sim) \
+            for ii, sim in enumerate(np.concatenate(sims))]
+    sims = sorted(sims, key=lambda x: x[-1], reverse=True)
+
+    # reorder the imagery to match the ranking
+    numImages = int((1.-percentile) * np.prod(imagery.shape[:2]))
+    sortedImagery = np.zeros([numImages] + list(imagery.shape[-3:]),
+                             dtype=imagery.dtype)
+    sortedConfidence = np.zeros([numImages], dtype=np.float32)
+    for counter, (ii, jj, sim) in enumerate(sims[:numImages]) :
+        sortedImagery[counter][:] = imagery[ii][jj][:]
+        sortedConfidence[counter] = sim
+
+    # dump the ranked result as a series of batches
+    if debug :
+        newNumBatch = int(math.ceil(numImages / batchSize))
+        newFlatShape = [newNumBatch * batchSize] + list(imagery.shape[-3:])
+        newBatchShape = [newNumBatch, batchSize] + list(imagery.shape[-3:])
+        dimdiff = tuple([(0, a - b) for a, b in zip(newFlatShape,
+                                                    sortedImagery.shape)])
+        tmp = np.pad(sortedImagery, dimdiff, 
+                     mode='constant', constant_values=0)
+        tmp = np.reshape(tmp, newBatchShape)
+        for ii in range(len(imagery)) :
+            saveTiledImage(imagery[ii], str(ii) + '.tif',
+                           imagery.shape[-2:])
+        for ii in range(len(tmp)) :
+            saveTiledImage(tmp[ii], str(ii) + '_sorted.tif',
+                           imagery.shape[-2:])
+
+    return sortedImagery, sortedConfidence
+
+if __name__ == '__main__' :
+    '''This application tests how close the examples are to a provided target
+       set. This averages the results against several networks and then
+       reorders the chips according to the most likeness to the target set.
+    '''
+    parser = argparse.ArgumentParser()
+    addLoggingParams(parser)
+    addUnsupDataParams(parser, 'saeClass', multiLoad=True)
+    parser.add_argument('--percentile', dest='percentile', type=float,
+                        default=.95, help='Keep the top percentile of ' +
+                        'information corresponding to the most likely matches')
+    parser.add_argument('--debug', dest='debug', type=bool, required=False,
+                        help='Drop debugging information about the runs.')
+    options = parser.parse_args()
+
+    # setup the logger
+    log, prof = setupLogging(options, 'SAE-Classification Benchmark')
+
+    # NOTE: The pickleDataset will silently use previously created pickles if
+    #       one exists (for efficiency). So watch out for stale pickles!
+    train, test, labels = ingestImagery(filepath=options.data, shared=False,
+                                        batchSize=options.batchSize,
+                                        holdoutPercentage=options.holdout,
+                                        log=log)
+
+    # load all networks initialized to the target imagery
+    nets = createNetworks(options.targetDir, options.maxTarget,
+                          options.batchSize, options.synapse,
+                          prof, options.debug)
+
+    # test the training data for similarity to the target
+    sortDataset(nets, test, options.percentile, options.debug)
